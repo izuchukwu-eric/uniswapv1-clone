a279812ad6b1247b50a3963c9d79c0a7fb153dd1
Izuchukwu Onukwube <57435925+izuchukwu-eric@users.noreply.github.com>
2022-07-30 02:55:12 +0100

    added eth to token swap

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -32,13 +32,14 @@ contract Exchange {
         return (inputReserve * 1000) / outputReserve;
     }
 
-    
+    /**function to get amount */
     function getAmount(uint256 inputAmount, uint256 inputReserve, uint256 outputReserve) private pure returns(uint256) {
         require(inputReserve > 0 && outputReserve > 0, "invalid reserves");
 
         return (inputAmount * outputReserve) / (inputReserve + inputAmount);
     }
 
+    //function to get token amount
     function getTokenAmount(uint256 _ethSold) public view returns (uint256) {
         require(_ethSold > 0, "ethSold is too small");
 
@@ -47,11 +48,22 @@ contract Exchange {
         return getAmount(_ethSold, address(this).balance, tokenReserve);
     }
 
+    /**function to get ether amount */
     function getEthAmount(uint256 _tokenSold) public view returns(uint256) {
         require(_tokenSold > 0, "tokenSold is too small");
 
         uint256 tokenReserve = getReserve();
 
         return getAmount(_tokenSold, tokenReserve, address(this).balance);
+    }
+
+    //function for swapping
+    function ethToTokenSwap(uint256 _minTokens) public payable {
+        uint256 tokenReserve = getReserve();
+        uint256 tokensBought = getAmount(msg.value, address(this).balance - msg.value, tokenReserve);
+
+        require(tokensBought >= _minTokens, "insufficient output amount");
+
+        IERC20(tokenAddress).transfer(msg.sender, tokensBought);
     }
 }
